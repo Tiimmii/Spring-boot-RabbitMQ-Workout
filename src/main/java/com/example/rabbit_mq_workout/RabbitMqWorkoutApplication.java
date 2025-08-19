ad2758bad34dc5fb923029ff44f68fa11eaff0c0
Timmi <daniel@anyhq.co>
2025-08-19 12:49:47 +0100

    First commit

---
+++ src/main/java/com/example/rabbit_mq_workout/RabbitMqWorkoutApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbit_mq_workout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqWorkoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMqWorkoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rabbit_mq_workout/RabbitMqWorkoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbit_mq_workout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqWorkoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
